fd53d0585d8640cb858cd03dea2f76949ffb843a
Karolina Jochymek <karolina.jochymek@euvic.pl>
2025-05-20 19:08:04 +0200

    base repo for homework

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,11 +2,9 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AppComponent]
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'wew-21-zadania-25' title`, () => {
+  it(`should have as title 'wew21-2023-zadania'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('wew-21-zadania-25');
+    expect(app.title).toEqual('wew21-2023-zadania');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, wew-21-zadania-25');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('wew21-2023-zadania app is running!');
   });
 });
